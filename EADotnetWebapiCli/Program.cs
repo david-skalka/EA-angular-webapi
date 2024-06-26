be74d7ad6d5b3dfed9277dc7c2a3dbedb1eab8d8
david.skalka <david.skalka@viklan.cz>
2024-06-26 16:31:46 +0200

    generator pipeline

--- EADotnetWebapiCli/Program.cs
+++ EADotnetWebapiCli/Program.cs
@@ -3,23 +3,12 @@
 using CommandLine;
 using EADotnetWebapiCli;
 using EADotnetWebapiCli.Templates;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Windows.Input;
 
 
-void ConfirmFileWrite(string path, string content)
-{
-    if (File.Exists(path))
-    {
-        Console.WriteLine("File "+ path + " already exists. Do you want to overwrite it? (y/n)");
-        var key = Console.ReadKey();
-        if (key.Key != ConsoleKey.Y)
-        {
-            Console.WriteLine("Aborted");
-        }
-    }
 
-    File.WriteAllText(path, content);
-
-}
 
 
 void ExecShell(string filename, string args, string? cwd)
@@ -37,50 +26,70 @@ void ExecShell(string filename, string args, string? cwd)
 Parser.Default.ParseArguments<InitializeOptions, DbContextOptions, EntityOptions>(args).MapResult(
     (InitializeOptions options) =>
 {
-    ExecShell("dotnet", "new webapi -f net8.0 -n " + options.ProjectName + " -o " + options.OutputDir, null);
-    Directory.CreateDirectory(Path.Combine(options.OutputDir, "Models"));
-    Directory.CreateDirectory(Path.Combine(options.OutputDir, "Controllers"));
-    ExecShell("dotnet", "add package Microsoft.EntityFrameworkCore --version 6.0.27", options.OutputDir);
-    ExecShell("dotnet", "add package Microsoft.EntityFrameworkCore.Sqlite --version 6.0.27", options.OutputDir);
+    var pipeline = new IGeneratorCommand[] {
+        new ShellGeneratorCommand("dotnet", "new webapi -f net8.0 -n " + options.ProjectName + " -o " + options.OutputDir, null),
+        new MkdirGeneratorCommand(Path.Combine(options.OutputDir, "Models")),
+        new MkdirGeneratorCommand(Path.Combine(options.OutputDir, "Controllers")),
+        new ShellGeneratorCommand("dotnet", "add package Microsoft.EntityFrameworkCore --version 6.0.27", options.OutputDir),
+        new ShellGeneratorCommand("dotnet", "add package Microsoft.EntityFrameworkCore.Sqlite --version 6.0.27", options.OutputDir),
+        new WriteCallbackResultGeneratorCommand(() => {
 
-    var programTemplate = new EADotnetWebapiCli.Templates.Program();
-    programTemplate.ProjectName = options.ProjectName;
-    var programContent = programTemplate.TransformText();
-    File.WriteAllText(Path.Combine(options.OutputDir, "Program.cs"), programContent);
+            var programTemplate = new EADotnetWebapiCli.Templates.Program();
+            programTemplate.ProjectName = options.ProjectName;
+            return programTemplate.TransformText();
+
+        }, Path.Combine(options.OutputDir, "Program.cs"))
+    };
+    Generate(pipeline);
     return 0;
 },
 (DbContextOptions options) =>
 {
+    var pipeline = new IGeneratorCommand[] {
+        new WriteCallbackResultGeneratorCommand(() =>
+        {
+            var dbContext = new DbContext();
+            dbContext.ProjectName = options.ProjectName;
+            dbContext.Entities = new EAXmiParser().Parse(options.Xmi).Where(x => x.Stereotype == "DotnetWebapi:Entity").Where(x => options.Entities.Split(",").Contains(x.Name)).ToArray();
+            return dbContext.TransformText();
 
-    var dbContext = new DbContext();
-    dbContext.ProjectName = options.ProjectName;    
-    dbContext.Entities = new EAXmiParser().Parse(options.Xmi).Where(x=>x.Stereotype=="DotnetWebapi:Entity").Where(x=> options.Entities.Split(",").Contains(x.Name)).ToArray();
-    var programContent = dbContext.TransformText();
-    ConfirmFileWrite(Path.Combine(options.OutputDir, "ApplicationDbContext.cs"), programContent);
+        }, Path.Combine(options.OutputDir, "ApplicationDbContext.cs"))
+    };
+    Generate(pipeline);
     return 0;
 },
 (EntityOptions options) =>
 {
     var parser = new EAXmiParser();
     var diagram = parser.Parse(options.Xmi);
-    
-    var efModel = new EfModel();
-    
-    
+
+   
+
+
     foreach (var entity in options.Entities.Split(","))
     {
-        efModel.Model = diagram.Single(x => x.Name == entity);
-        efModel.ProjectName = options.ProjectName;
-        var efModelContent = efModel.TransformText();
 
-        ConfirmFileWrite(Path.Combine(options.OutputDir, "Models", entity + ".cs"), efModelContent);
+        var pipeline = new IGeneratorCommand[] {
+        new WriteCallbackResultGeneratorCommand(() =>
+        {
+             var efModel = new EfModel();
+           efModel.Model = diagram.Single(x => x.Name == entity);
+           efModel.ProjectName = options.ProjectName;
+           return efModel.TransformText();
 
-        var controller = new Controller();
-        controller.Model = diagram.Single(x => x.Name == entity);
-        controller.ProjectName = options.ProjectName;
-        var controllerContent = controller.TransformText();
+        }, Path.Combine(options.OutputDir, "Models", entity + ".cs")),
 
-        ConfirmFileWrite(Path.Combine(options.OutputDir, "Controllers", entity + "Controller.cs"), controllerContent);
+                new WriteCallbackResultGeneratorCommand(() =>
+        {
+                 var controller = new Controller();
+                controller.Model = diagram.Single(x => x.Name == entity);
+                controller.ProjectName = options.ProjectName;
+                return controller.TransformText();
+
+
+        }, Path.Combine(options.OutputDir,  "Controllers", entity + "Controller.cs"))
+    };
+        Generate(pipeline);
 
     }
 
@@ -93,6 +102,106 @@ Parser.Default.ParseArguments<InitializeOptions, DbContextOptions, EntityOptions
 , errors => 1);
 
 
+void Generate(IGeneratorCommand[] pipeline)
+{
+    foreach (var command in pipeline)
+    {
+        command.Execute();
+    }
+
+}
+
+public interface IGeneratorCommand
+{
+    void Execute();
+}
+
+
+public class WriteCallbackResultGeneratorCommand : IGeneratorCommand
+{
+    public Func<string> callback;
+
+    public string path;
+
+
+    public WriteCallbackResultGeneratorCommand(Func<string> callback, string path)
+    {
+        this.callback = callback;
+        this.path = path;
+    }
+
+    public void Execute()
+    {
+        if (File.Exists(path))
+        {
+            Console.WriteLine("File " + path + " already exists. Do you want to overwrite it? (y/n)");
+            var key = Console.ReadKey();
+            if (key.Key != ConsoleKey.Y)
+            {
+                Console.WriteLine("Aborted");
+            }
+        }
+        File.WriteAllText(path, callback());
+    }
+}
+
+
+public class ShellGeneratorCommand : IGeneratorCommand
+{
+    private string filename;
+    private string args;
+    private readonly string? cwd;
+
+    public ShellGeneratorCommand(string filename, string args, string? cwd)
+    {
+        this.filename = filename;
+        this.args = args;
+        this.cwd = cwd;
+    }
+
+    public void Execute()
+    {
+        var process = new System.Diagnostics.Process();
+        process.StartInfo.FileName = filename;
+        process.StartInfo.Arguments = args;
+        process.StartInfo.WorkingDirectory = cwd;
+        process.Start();
+        process.WaitForExit();
+    }
+}
+
+
+public class RmGeneratorCommand : IGeneratorCommand
+{
+    private string filename;
+
+    public RmGeneratorCommand(string filename)
+    {
+        this.filename = filename;
+    }
+
+    public void Execute()
+    {
+        File.Delete(filename);
+    }
+}
+
+
+
+public class MkdirGeneratorCommand : IGeneratorCommand
+{
+    private string path;
+
+    public MkdirGeneratorCommand(string path)
+    {
+        this.path = path;
+    }
+
+    public void Execute()
+    {
+        Directory.CreateDirectory(path);
+    }
+}
 
 [Verb("initialize", HelpText = "Initialize project")]
 class InitializeOptions
