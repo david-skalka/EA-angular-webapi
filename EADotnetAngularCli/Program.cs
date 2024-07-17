be8d6d847f5a168d9bbf9e258a6b77b1605af001
David Skalka <david.skalka@gmail.com>
2024-07-17 07:58:13 +0200

    runner test

--- EADotnetAngularCli/Program.cs
+++ EADotnetAngularCli/Program.cs
@@ -26,7 +26,6 @@ Parser.Default.ParseArguments<InitializeApiOptions, InitializeClientOptions, DbC
 {
     var outputDir = Path.Combine(Directory.GetCurrentDirectory(), options.OutputDir);
 
-    
 
     var testProjectPath = Path.Combine(outputDir, options.ProjectName + "IntegrationTest");
 
